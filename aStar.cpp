5f852b88ee5f83e6dd318bc62e393baf71b81b8e
chufam <theinsurgentarchive@gmail.com>
2025-03-12 11:02:08 -0700

    Moved Node Class outside of Grid Class, added getters

--- aStar.cpp
+++ aStar.cpp
@@ -1,38 +1,30 @@
 #include "aStar.h"
 
-Grid::Node::Node()
+Node::Node()
 {
     //Initialize Variables
     obstacle = false;
     visited = false;
-    pos[0] = 0;
-    pos[1] = 0;
+    init_dist = 0;
+    goal_dist = 0;
+    parent = nullptr;
 }
 
-Grid::Node::Node(int x, int y)
-{
-    //Initialize Variables
-    obstacle = false;
-    visited = false;
-    pos[0] = x;
-    pos[1] = y;
-}
-
-Grid::Node::Node(int x, int y, bool isObstacle)
+Node::Node(bool isObstacle)
 {
     //Initialize Variables
     obstacle = isObstacle;
     visited = false;
-    pos[0] = x;
-    pos[1] = y;
-
+    init_dist = 0;
+    goal_dist = 0;
+    parent = nullptr;
 }
 
 Grid::Grid()
 {
     //Initialize Variables
-    size[0] = 10;
-    size[1] = 10;
+    grid_size[0] = 10;
+    grid_size[1] = 10;
 
     //Initialize Grid Nodes
     initGrid();
@@ -40,8 +32,8 @@ Grid::Grid()
 
 Grid::Grid(int x_size, int y_size)
 {
-    size[0] = x_size;
-    size[1] = y_size;
+    grid_size[0] = x_size;
+    grid_size[1] = y_size;
 
     //Initalize Grid Nodes
     initGrid();
@@ -56,37 +48,105 @@ void Grid::setObstacle(int x, int y)
 //Return Grid's X-Axis Size
 int Grid::getSizeX()
 {
-    return size[0];
+    return grid_size[0];
 }
 
 //Return Grid's Y-Axis Size
 int Grid::getSizeY()
 {
-    return size[1];
+    return grid_size[1];
+}
+
+//Retrieves The Node requested in the Grid
+Node* Grid::getNode(int x, int y)
+{
+    return &node_grid[x][y];
 }
 
 //Generate Node Grid, Set Node Positions
 void Grid::initGrid()
 {
-    node_grid = new Node *[size[0]];
-    for (int i = 0; i < size[0]; i++) {
-        node_grid[i] = new Node [size[1]];
-        for (int j = 0; j < size[1]; j++) {
-            node_grid[i][j].pos[0] = i;
-            node_grid[i][j].pos[1] = j;
+    //Generate Nodes
+    node_grid = new Node *[grid_size[0]];
+    for (int i = 0; i < grid_size[0]; i++) {
+        node_grid[i] = new Node [grid_size[1]];
+    }
+
+    //Fill Each Node's Neighbors Vector Matrix
+    for (int x = 0; x < grid_size[0]; x++) {
+        for (int y = 0; y < grid_size[1]; y++) {
+            
+            //Load All Y-Axis Neighbors into The Node's Vector Matrix
+            if (y > 0) {
+                node_grid[x][y].neighbors.push_back(
+                    &node_grid[x][y - 1]
+                );
+            }
+            if (y < grid_size[1] - 1) {
+                node_grid[x][y].neighbors.push_back(
+                    &node_grid[x][y + 1]
+                );
+            }
+
+            //Load All X-Axis Neighbors into The Node's Vector Matrix
+            if (x > 0) {
+                node_grid[x][y].neighbors.push_back(
+                    &node_grid[x - 1][y]
+                );
+            }
+            if (x < grid_size[0] - 1) {
+                node_grid[x][y].neighbors.push_back(
+                    &node_grid[x + 1][y]
+                );
+            }
+
+            //Load All Diagonal Neighbors into The Node's Vector Matrix
+            if (x > 0 && y > 0) {
+                node_grid[x][y].neighbors.push_back(
+                    &node_grid[x - 1][y - 1]
+                );
+            }
+            if (x > 0 && y < grid_size[1] - 1) {
+                node_grid[x][y].neighbors.push_back(
+                    &node_grid[x - 1][y + 1]
+                );
+            }
+            if (x < grid_size[0] - 1 && y > 0) {
+                node_grid[x][y].neighbors.push_back(
+                    &node_grid[x + 1][y - 1]
+                );
+            }
+            if (x < grid_size[0] - 1 && y < grid_size[1] - 1) {
+                node_grid[x][y].neighbors.push_back(
+                    &node_grid[x + 1][y + 1]
+                );
+            }
         }
     }
+    
 }
 
 Grid::~Grid()
 {
-    for (int i = 0; i < size[0]; i++) {
+    for (int i = 0; i < grid_size[0]; i++) {
         delete [] node_grid[i];
     }
     delete [] node_grid;
 }
 
-void aStar(Grid grid, int begin[2], int ending[2])
+void aStar(Grid grid, int begin_node[], int ending_node[])
 {
+    //Retrieve Pointer to Start Node
+    Node* start = grid.getNode(
+        begin_node[0],
+        begin_node[1]
+    );
+
+    //Retrieve Pointer to Goal Node
+    Node* goal = grid.getNode(
+        ending_node[0],
+        ending_node[1]
+    );
+
 
 }
--- aStar.h
+++ aStar.h
@@ -1,40 +1,38 @@
 #include <vector>
 
+class Node
+{
+    public:
+        //Node Conditionals
+        bool obstacle, visited;
+        
+        //Node Distance from Initial/Goal Node
+        float init_dist;
+        float goal_dist;
+        
+        //Vector Matrix of Neighboring Nodes
+        std::vector<Node*> neighbors;
+        
+        //Node Directly Preceding Current Node
+        Node* parent;
+        
+        //Constructor
+        Node();
+        Node(bool);
+
+        //Function
+};
+
 //Grid of Node Elements, Used in A* Search
-class Grid
+class Grid 
 {
     private:
         //Grid's X & Y Axis Size
-        int size[2];
-
-        //Grid Element
-        class Node
-        {
-            public:
-                //Node Conditionals
-                bool obstacle, visited;
-                
-                //Node Distance from Initial/Goal Node
-                float dist[2];
-
-                //Node Grid Position
-                int pos[2];
-
-                //Vector Matrix of Neighboring Nodes
-                std::vector<Node*> neighbors;
-
-                //Node Directly Preceding Current Node
-                Node* parent;
-
-                //Constructor
-                Node();
-                Node(int, int);
-                Node(int, int, bool);
-        };
+        int grid_size[2];
     public:
         //Dynamic Node Grid
         Node** node_grid;
-        
+
         //Constructor
         Grid();
         Grid(int, int);
@@ -43,11 +41,11 @@ class Grid
         void setObstacle(int, int);
         int getSizeX();
         int getSizeY();
+        Node* getNode(int, int);
         void initGrid();
-
         //Destructor
         ~Grid();
 };
-
+        
 //A* Search Algorithm
 void aStar(Grid, int[2], int[2]);
